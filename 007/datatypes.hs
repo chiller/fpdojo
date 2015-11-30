7dfba00df0391fb03a8da6ecf505b70df2f5bff3
Damian Soriano <damiansoriano@gmail.com>
2015-11-30 12:44:16 +0000

    Define first function for MyBool

--- 007/datatypes.hs
+++ 007/datatypes.hs
@@ -1,2 +1,10 @@
 data MyBool = MyFalse | MyTrue
     deriving (Show)
+
+myNot :: MyBool -> MyBool
+myNot MyTrue = MyFalse
+myNot MyFalse = MyTrue
+
+myOr :: MyBool -> MyBool -> MyBool
+myOr MyTrue _ = MyTrue
+myOr MyFalse x = x
